ae19a4b5ef6649fae90476f303e8e69d2f38bbd2
Habib Vadillo <habibvar@gmail.com>
2021-07-18 17:04:28 +0200

    nothing significant

--- server/config/index.js
+++ server/config/index.js
@@ -1,19 +1,11 @@
 const express = require("express");
 
-// ℹ️ Responsible for the messages you see in the terminal as requests are coming in
-// https://www.npmjs.com/package/morgan
 const logger = require("morgan");
 
-// ℹ️ Needed to accept from requests from 'the outside'. CORS stands for cross origin resource sharing
-// unless the request if from the same domain, by default express wont accept POST requests
 const cors = require("cors");
 
 // Middleware configuration
 module.exports = (app) => {
-  // Because this is a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
-  // Services like heroku use something called a proxy and you need to add this to your server
-  app.set("trust proxy", 1);
-
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
